73586ae7e289b57b8b95d373651e99eeb13ab047
“thaipvtph06394” <thaipvtph06394@fpt.edu.vn>
2019-03-13 12:39:50 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/poly/perfume_manager/HelloActivity.java
@@ -0,0 +1,13 @@
+package vn.edu.poly.perfume_manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+    }
+}
