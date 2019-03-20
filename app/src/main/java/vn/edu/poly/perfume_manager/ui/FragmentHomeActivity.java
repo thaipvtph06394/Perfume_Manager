72756eb047b51823610d977e90b865723b1cc713
“thaipvtph06394” <thaipvtph06394@fpt.edu.vn>
2019-03-20 20:09:04 +0700

    HT giao diện 70%

--- app/src/main/java/vn/edu/poly/perfume_manager/ui/FragmentHomeActivity.java
+++ app/src/main/java/vn/edu/poly/perfume_manager/ui/FragmentHomeActivity.java
@@ -1,15 +1,22 @@
 package vn.edu.poly.perfume_manager.ui;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import vn.edu.poly.perfume_manager.R;
 
-public class FragmentHomeActivity extends AppCompatActivity {
+public class FragmentHomeActivity extends Fragment {
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment_home);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_fragment_home,container,false);
+        return view;
     }
 }
